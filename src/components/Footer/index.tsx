91cbf1b54ffa09520da81f0ece55e6b06b96c3ed
Valentin Lica <licavalentinh@gmail.com>
2022-07-27 20:19:21 +0200

    small changes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 
+import { open } from "@tauri-apps/api/shell";
+
 import styles from "@styles/components/Footer/index.module.scss";
 
 const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
-      <p>© {new Date().getFullYear()} Autoshkolla. All rights reserved.</p>
+      <p>
+        Developed with <span className={styles.heart} /> by
+        <span
+          onClick={async () => {
+            await open("https://github.com/ValentinHLica");
+          }}
+        >
+          Valentin Lica
+        </span>
+      </p>
     </footer>
   );
 };
